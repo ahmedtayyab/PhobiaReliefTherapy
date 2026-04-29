c1892748dae2f284702ad12e6073d53fa5034902
Ahmad Tayyab <127379340+ahmedtayyab@users.noreply.github.com>
2026-04-29 21:56:30 +0500

    Wired UI, Canvases, and C# Managers for all scenes

--- Assets/Scripts/Managers/userData.cs
+++
@@ -1,7 +0,0 @@
-public static class UserData
-{
-    public static string userName;
-    public static string selectedPhobia;
-    public static string selectedDifficulty;
-    public static float baselineHeartRate;
-}
